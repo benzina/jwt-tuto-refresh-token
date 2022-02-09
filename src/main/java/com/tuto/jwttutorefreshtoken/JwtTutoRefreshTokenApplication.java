76f1d33be636dc1da9671f7dbc53ab2eed5839f8
EL MAHDI BENZINA <benzina.elmahdi@gmail.com>
2022-02-09 22:23:50 +0100

    Initial commit

---
+++ src/main/java/com/tuto/jwttutorefreshtoken/JwtTutoRefreshTokenApplication.java
@@ -0,0 +1,13 @@
+package com.tuto.jwttutorefreshtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTutoRefreshTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTutoRefreshTokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuto/jwttutorefreshtoken/JwtTutoRefreshTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuto.jwttutorefreshtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutoRefreshTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
